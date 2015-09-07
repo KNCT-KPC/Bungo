61f054364a77ac7c11234167650c2b33a5186cc7
cafeunder <cafe3641@gmail.com>
2015-09-08 00:02:59 +0900

    配置点計算実装した。明日本実装（予定）

--- Client/FullSearch/FullSearch/FullSearch.cpp
+++ Client/FullSearch/FullSearch/FullSearch.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <vector>
 
 char DEBUG_C;
 #define	STONE(n, x, y)	stones[((n) << 6) + ((y) << 3) + (x)]
@@ -36,6 +37,49 @@ void DEBUG_printMapStone(const int* stones, const int n){
 	}
 }
 
+class PutPoint{
+private:
+	int k;
+	std::vector<int> p;
+
+	int i;
+
+public :
+	PutPoint(const int k, const int* shit, const int* nearest){
+		for(int s = 0; shit[s] != -1; s++){
+			for(int n = 0; nearest[n] != -1; n++){
+				int value = nearest[n] - shit[s];
+				for(int i = 0; i < p.size(); i++){
+					if(value == p[i]){
+						goto NEXT;
+					}
+				}
+				p.push_back(value);
+
+			NEXT:;
+			}
+		}
+
+		i = 0;
+		this->k = k;
+	}
+
+	const int* GetPointAry(){
+		return &(p[0]);
+	}
+	bool GetNextValue(int* value){
+		if(i < p.size()){
+			*value = p[i];
+			i++;
+			return true;
+		}
+
+		return false;
+	}
+	int GetShitNumber(){
+		return k;
+	}
+};
 
 void FullSearch(const int* Map, const int x1, const int y1, const int x2, const int y2, const int* stones, int stonesNum, char* solution){
 	//strcat(solution, "H 0 2 2\n")
@@ -46,6 +90,7 @@ void FullSearch(const int* Map, const int x1, const int y1, const int x2, const 
 
 		//わかりやすくマップ定義
 	int* map = new int[width * height];	//対象領域だけを考慮したマップ
+	std::vector<int> startNearest;
 	for(int h = 0; h < height; h++){
 		for(int w = 0; w < width; w++){
 			int value = Map[(w+x1) + (h+y1)*32];
@@ -53,11 +98,13 @@ void FullSearch(const int* Map, const int x1, const int y1, const int x2, const 
 			//入力では 壁 = 1 になっているが、ここでは 壁 = -1 とする。
 			if(value == 0){
 				map[w + h*width] = 0;
+				startNearest.push_back(w+h*width);
 			} else {
 				map[w + h*width] = -1;
 			}
 		}
 	}
+	startNearest.push_back(-1);	//-1は番兵
 
 	printf("%d %d\n", width, height); //DEBUG
 	DEBUG_printMap(map, width, height);
@@ -86,10 +133,14 @@ void FullSearch(const int* Map, const int x1, const int y1, const int x2, const 
 		printf("\tstone %d\n", 1);
 		DEBUG_printMapStone(stones, s);
 		DEBUG_printBaseAryStone(shitAry[s]);
-		scanf("%c", &DEBUG_C);
+		//scanf("%c", &DEBUG_C);
 		//DEBUG
 	}
 
+
+	//-----探索-----//（長い）
+	int kn = 0;
+	PutPoint points(kn, shitAry[kn], &(startNearest[0]));
 
 	//-----終了処理-----//
 	delete map;

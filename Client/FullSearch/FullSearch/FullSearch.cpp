8a90e782fb2ab6db3faadd518bad0b7e60156a56
cafeunder <cafe3641@gmail.com>
2015-09-11 22:32:31 +0900

    反転・回転なし＆解答送信プロトコル未実装

--- Client/FullSearch/FullSearch/FullSearch.cpp
+++ Client/FullSearch/FullSearch/FullSearch.cpp
@@ -24,7 +24,6 @@ void DEBUG_printMap(const int* map, const int width, const int height){
 		}
 		printf("\n");
 	}
-	printf("\n");
 }
 
 void DEBUG_printPutShitOnMap(const int* map, const int* putPoints, const int ppNum, const int width, const int height){
@@ -71,13 +70,41 @@ void DEBUG_waitKey(){
 
 class PutPoint{
 private:
+	int i;
 	int k;
 	std::vector<int> p;
 	int* neighborAry;
-
-	int i;
+	bool start;
 
 public :
+	PutPoint(const int k, const int* shit, PutPoint* prevP){
+		int size;
+		for(size = 0; prevP->neighborAry[size] != -1; size++);
+
+		neighborAry = new int[size+1];
+		for(int i = 0; i < size; i++){
+			neighborAry[i] = prevP->neighborAry[i];
+		}
+		neighborAry[size] = -1;
+
+		for(int s = 0; shit[s] != -1; s++){
+			for(int n = 0; neighborAry[n] != -1; n++){
+				int value = neighborAry[n] - shit[s];
+				for(int i = 0; i < p.size(); i++){
+					if(value == p[i]){
+						goto NEXT;
+					}
+				}
+				p.push_back(value);
+
+			NEXT:;
+			}
+		}
+
+		i = 0;
+		this->k = k;
+		this->start = false;
+	}
 	PutPoint(const int k, const int* shit, const int* neighbor, const int neighborNum){
 		for(int s = 0; shit[s] != -1; s++){
 			for(int n = 0; neighbor[n] != -1; n++){
@@ -93,14 +120,15 @@ public :
 			}
 		}
 
-		i = 0;
-		this->k = k;
-
 		neighborAry = new int[neighborNum+1];
 		for(int i = 0; i < neighborNum; i++){
 			neighborAry[i] = neighbor[i];
 		}
 		neighborAry[neighborNum] = -1;
+
+		i = 0;
+		this->k = k;
+		this->start = false;
 	}
 	PutPoint(const int k, const int* shit, const int* startMap){
 		for(int s = 0; shit[s] != -1; s++){
@@ -117,11 +145,12 @@ public :
 			}
 		}
 
-		i = 0;
-		this->k = k;
-
 		neighborAry = new int[1];
 		neighborAry[0] = -1;
+
+		i = 0;
+		this->k = k;
+		this->start = true;
 	}
 	~PutPoint(){
 		delete neighborAry;
@@ -143,6 +172,9 @@ public :
 
 		return false;
 	}
+	bool isStart(){
+		return start;
+	}
 	int GetShitNumber(){
 		return k;
 	}
@@ -163,6 +195,16 @@ void PutShit(int* map, const int shitNumber, const int* putPoints, const int put
 	for(int i = 0; i < putAryLength; i++){
 		map[putPoints[i]] = shitNumber+1;
 	}
+}
+
+int CalcScore(int* map, int width, int height){
+	int score = 0;
+	for(int i = 0; i < (width+1)*height; i++){
+		if(map[i] == 0){
+			score++;
+		}
+	}
+	return score;
 }
 
 bool DEBUG_JudgePutable(const int* map, const int* shit, const int basePoint, const int width, const int height, int* putPoints, int* putAryLength){
@@ -224,7 +266,9 @@ bool JudgeExistValue(const int* ary, const int aryNum, const int value){
 std::vector<int>* CalcNeighborPoint(const int* map, const int* prevNeighbor, const int* putPoints, const int putAryLength, const int width, const int height){
 	std::vector<int> *neighbor = new std::vector<int>();
 	for(int i = 0; prevNeighbor[i] != -1; i++){
-		neighbor->push_back(prevNeighbor[i]);
+		if(map[prevNeighbor[i]] == 0){	
+			neighbor->push_back(prevNeighbor[i]);
+		}
 	}
 
 	for(int i = 0; i < putAryLength; i++){
@@ -308,13 +352,15 @@ void FullSearch(const int* Map, const int x1, const int y1, const int x2, const 
 		}
 
 		//DEBUG
-		printf("\tstone %d\n", 1);
+		printf("\tstone %d\n", s);
 		DEBUG_printMapStone(stones, s);
 		DEBUG_printBaseAryStone(shitAry[s]);
 		//DEBUG
 	}
 	printf("\n");	//DEBUG
 
+	printf("\nStart : \n");	//DEBUG
+	DEBUG_waitKey();
 
 	//-----íTçı-----//Åií∑Ç¢Åj
 	int kn = 0;
@@ -324,8 +370,14 @@ void FullSearch(const int* Map, const int x1, const int y1, const int x2, const 
 	int putPoints[17];
 	int putAryLength;
 
+	int minScore = 1025;
+	int putShitNum = 0;
+	int minScore_minShitNum = 1025;
+
 	//Ç±Ç±Ç©ÇÁåJÇËï‘Çµ
 	while(1){
+		//DEBUG_printMap(map, width+1, height); printf("\n");
+
 		PutPoint* p = pStack.top();
 		kn = p->GetShitNumber();
 		const int* shit = shitAry[kn];
@@ -334,40 +386,101 @@ void FullSearch(const int* Map, const int x1, const int y1, const int x2, const 
 
 		int nextBasePoint;
 		if(!p->GetNextValue(&nextBasePoint)){
-			break;	//DEBUG
-			if(pStack.empty()){
-				//áLÇ÷
+			if(pStack.size() == 1){
+				break;
 			} else {
-				//áIÇ÷
+				//printf("\nÅÅÅÅÅÅÅÅÅÅ cannot place No.%d ÅÅÅÅÅÅÅÅÅÅ\n", kn);
+
+				pStack.pop();
+				if(kn+1 >= stonesNum){
+					goto TERMINAL;
+				}
+
+				if(p->isStart()){
+					pStack.push(new PutPoint(kn+1, shitAry[kn+1], &(startNeighbor[0])));
+				} else {
+					pStack.push(new PutPoint(kn+1, shitAry[kn+1], p));
+				}
+				/*
+				printf("Å´Å´Å´Å´Å´ next shit Å´Å´Å´Å´Å´\n");
+				DEBUG_waitKey();
+				*/
+				continue;
 			}
 		}
 
+		/*
 		printf("\tput point : %d\n", nextBasePoint);
 		DEBUG_printBaseAryStone(shit);
+		*/
 
-		//JudgePutable(map, shit, nextBasePoint, width, height, putPoints, &putAryLength);
-		if(DEBUG_JudgePutable(map, shit, nextBasePoint, width, height, putPoints, &putAryLength)){
+		bool result = JudgePutable(map, shit, nextBasePoint, width, height, putPoints, &putAryLength);
+		/*
+		bool result = DEBUG_JudgePutable(map, shit, nextBasePoint, width, height, putPoints, &putAryLength);
+		DEBUG_printPutShitOnMap(map, putPoints, putAryLength, width, height);
+		*/
+		
+		if(result){
+			//printf("\nplace > ");
+			
 			PutShit(map, kn, putPoints, putAryLength);
+			putShitNum++;
 
 			if(kn+1 >= stonesNum){
-				//áKÇ÷
+				goto TERMINAL;
 			}
 
 			std::vector<int>* neighbor = CalcNeighborPoint(map, p->GetNeighborAry(), putPoints, putAryLength, width, height); 
 
-			for(int i = 0; i < neighbor->size(); i++){
+			/* DEBUG
+			printf("neighbor[ ");
+			for(int i = 0; i < neighbor->size()-1; i++){
 				printf("%d ", neighbor->at(i));
 			}
-			printf("\n");
+			printf("]\n\n");
+
 			DEBUG_printMap(map, width+1, height);
-			
+			*/
+
 			pStack.push(new PutPoint(kn+1, shitAry[kn+1], neighbor->data(), neighbor->size()));
 			delete neighbor;
+
+			/* DEBUG
+			printf("\nÅ´Å´Å´Å´Å´ next shit Å´Å´Å´Å´Å´\n");
+			DEBUG_waitKey();
+			*/
 		} else {
-			printf("\t\tcannot place\n");
+			//printf("\ncannot place\n");
 		}
-		DEBUG_printPutShitOnMap(map, putPoints, putAryLength, width, height);
+
+		continue;
+	TERMINAL:
+		//É|ÉCÉìÉgèWåvÅAâìöçÏê¨
+		int score = CalcScore(map, width, height);
+		if(minScore > score){
+			minScore = score;
+			minScore_minShitNum = putShitNum;
+		} else if(minScore == score){
+			if(minScore_minShitNum > putShitNum){
+				minScore_minShitNum = putShitNum;
+			}
+		}
+
+		if(putShitNum == 3 && score == 0){
+			DEBUG_printMap(map, width+1, height);
+		}
+		/*
+		printf("\n\t !!!!! terminal !!!!!\n\n");
+		DEBUG_printMap(map, width+1, height);
+		printf("\n\t   score : %d\n", score);
+		printf("\t   shitNum : %d\n", putShitNum);
+
+		printf("Å™Å™Å™Å™Å™ preview shit Å™Å™Å™Å™Å™\n");
 		DEBUG_waitKey();
+		*/
+		putShitNum--;
+
+		continue;
 	}
 
 	//Ç±Ç±Ç‹Ç≈åJÇËï‘Çµ
@@ -376,6 +489,8 @@ void FullSearch(const int* Map, const int x1, const int y1, const int x2, const 
 	//-----èIóπèàóù-----//
 	delete map;
 	delete[] shitAry;
+
+	printf("\nEND : score(%d,%d)\n", minScore, minScore_minShitNum);
 
 	//DEBUG
 	DEBUG_waitKey();

edbbec871f2353cc2d2f122839e59d2e51a964db
lrks <yuke1222@gmail.com>
2015-09-21 02:51:06 +0900

    171行目の処理が不要だけど、残したかったので一旦commit

--- Client/MilkT/milkt.c
+++ Client/MilkT/milkt.c
@@ -25,6 +25,177 @@
 void dump(int *map, int x1, int y1, int x2, int y2, int *stones, int n);
 void stone2satstone(int8_t *sat_stones, const int *map_base, int x1, int y1, int x2, int y2, const int *stones_base, int n);
 
+
+
+void BlockDefineOperation(const int8_t *zk, int8_t *dst)
+{
+	int i, j, k;
+	
+	// Front and Back
+	memcpy(dst, zk, sizeof(int8_t) << 5);
+	for (i=0; i<32; i+=2) {
+		dst[32 + i] = -zk[i];
+		dst[32 + i+1] = zk[i+1];
+	}
+	
+	// Rotate
+	for (i=2; i<8; i++) {
+		int idx = (i << 5);
+		for (j=0; j<32; j+=2) {
+			dst[idx + j] = -dst[idx - 63 + j];
+			dst[idx + j + 1] = dst[idx - 64 + j];
+		}
+	}
+
+	// dedup
+	int k;
+	for (i=1; i<8; i++) {
+		int m_idx = i << 5;
+
+		for (j=0; j<i; j++) {
+			int s_idx = j << 5;
+			if (dst[s_idx] == 0xff) continue;
+
+			for (k=0; k<30; k++) {
+				if (dst[m_idx + k] != dst[s_idx + j]) goto NO_DUP;
+			}
+		}
+
+		// dup
+		dst[m_idx] = 0xff;
+
+		// no
+	NO_DUP:
+		continue; // noop
+	}
+
+	return len;
+}
+
+
+
+
+void createCSPfile(FILE *fp, int *map, int x1, int y1, int x2, int y2, int8_t *sat_stones, int n)
+{
+	int x,y, i, j, k, l;
+
+	// Mass Define
+	fprintf(fp, "(domain zk 0 %d)\n", n - 1);
+	fprintf(fp, "(domain obstacle %d)\n", n);
+	
+	for(y=(y1-1); y<=y2; y++) {
+		for(x=(x1-1); x<=x2; x++) {
+			fprintf(fp, "(int x_%d_%d %s)\n", x, y, (!(y1 <= y < y2) || !(x1 <= x < x2) || MAP(x, y) == 1) ? "obstacle" : "zk");
+		}
+	}
+
+
+	// Block Define
+	fprintf(fp, "(domain intbool 0 1)\n");
+	for (y=y1; y<y2; y++) {
+		for (x=x1; x<x2; x++) {
+			for (i=0; i<n; i++) {
+				fprintf(fp, "(int y_%d_%d_%d)\n", x, y, i);
+			}
+		}
+	}
+
+	fprintf(fp, "(predicate (onlyonesub a0 a1 a2 a3 a4 a5 a6 a7) (and a0 (not a1) (not a2) (not a3) (not a4) (not a5) (not a6 )(not a7)))\n");
+	fprintf(fp, "(predicate (onlyone b0 b1 b2 b3 b4 b5 b6 b7) (or (onlyonesub b0 b1 b2 b3 b4 b5 b6 b7) (onlyonesub b1 b2 b3 b4 b5 b6 b7 b0) (onlyonesub b2 b3 b4 b5 b6 b7 b0 b1) (onlyonesub b3 b4 b5 b6 b7 b0 b1 b2) (onlyonesub b4 b5 b6 b7 b0 b1 b2 b3) (onlyonesub b5 b6 b7 b0 b1 b2 b3 b4) (onlyonesub b6 b7 b0 b1 b2 b3 b4 b5) (onlyonesub b7 b0 b1 b2 b3 b4 b5 b6)))\n");
+
+	for (y=y1; y<y2; y++) {
+		for (x=x1; x<x2; x++) {
+			for (i=0; i<n; i++) {
+				int8_t tmp[256];
+				BlockDefineOpration(&sat_stones[i << 5], tmp);	// Vivid Red Operation
+
+				int len = 8;
+				fprintf(fp, "(iff (= y_%d_%d_%d 1) (onlyone", x, y, i);
+
+				for (j=0; j<8; j++) {
+					int idx = j << 5;
+					if (tmp[idx] == 0xff) continue;
+
+					fpos_t pos = fgetpos(fp, &pos);
+					fprintf(fp, " (and");
+
+					int o_x = 0, o_y = 0;
+					do {
+						int a_x = x + o_x;
+						int a_y = y + o_y;
+						if (!(y1 <= y < y2) || !(x1 <= x < x2)) goto DAMEDESU;
+						fprintf(fp, " (= x_%d_%d %d)", a_x, a_y, i);
+					} while (o_x != 0 || o_y != 0);
+
+					fprintf(fp, ")");
+					len--;
+					continue;
+
+				DAMEDESU:
+					fsetpos(fp, &pos);	// これ以降を0埋めするとかはないので、ある程度長い文を書いていたら困る
+				}
+
+				for (j=0; j<len; j++) fprintf(" false");
+				fprintf(fp, "))\n");
+			}
+		}
+	}
+
+
+	// Only one railgun
+	for (i=0; i<n; i++) {
+		fprintf(fp, "(<= (+");
+		for (y=y1; y<y2; y++) {
+			for (x=x1; x<x2; x++) {
+				printf(fp, " y_%d_%d_%d", x, y, i);
+			}
+		}
+		fprintf(fp, ") 1)\n");
+	}
+
+
+	// Order
+	fprintf(fp, "(int first_zk 0 %d)\n", n - 1);
+	for (i=0; i<n; i++) {
+		fprintf(fp, "(iff (= first_zk %d) (and", i);
+		for (y=y1; y<y2; y++) {
+			for (x=x1; x<x2; x++) {
+				fprintf(fp, " (>= x_%d_%d %n)", x, y, i);
+
+				int dx[] = {1, 0, -1, 0};
+				int dy[] = {0, 1, 0, -1};
+				fprintf(fp, "(=> (= x_%d_%d %d) (or", x, y, i);
+				for (k=0; k<4; k++) fprintf(fp, " (>= x_%d_%d %d)", x+dx[k], y+dy[k], n);
+				fprintf(fp, "))");
+
+				for (k=i+1; k<n; k++) {
+					for (l=0; l<4; l++) {
+						fprintf(fp, "(=> (and (= x_%d_%d %d) (= x_%d_%d %d))", x, y, i, x+dx[l], y+dx[l], k);
+						int xd, yd;
+						for (yd=y1; yd<y2; yd++){
+							for (xd=x1; xd<x2; xd++) {
+								fprintf(fp, " (or (>= %d x_%d_%d) (>= x_%d_%d %d))", i, xd, yd, xd, yd, k);
+							}
+						}
+					}
+				}
+
+			}
+		}
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
 int solver(FILE *fp, int8_t *sat_stones, int *map, int x1, int y1, int x2, int y2, int *stones, int n)
 {
 	x2++; y2++;
@@ -33,6 +204,8 @@ int solver(FILE *fp, int8_t *sat_stones, int *map, int x1, int y1, int x2, int y
 	stone2satstone(sat_stones, map, x1, y1, x2, y2, stones, n);
 
 	// Z to CSP
+
+
 	/*
 	int col = x2 - x1;
 	int row = y2 - y1;
